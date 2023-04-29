c677da48ad81aba003cb15677510a9cee3eb6fbf
Raghupathi-thanda <raghupathit281@gmail.com>
2023-04-29 16:50:54 +0530

    JavaScript_code

---
+++ src/scripts/index1.js
@@ -0,0 +1,6 @@
+function body(){
+    window.document.images[0].src = "./images/fish.jpg";
+    window.document.images[0].width = 200;
+    window.document.forms[0].elements[1].value = "Register";
+    window.document.forms[1].elements[1].value = "Login";
+}
---
+++ src/scripts/index2.js
@@ -0,0 +1,6 @@
+function body(){
+    pict.src = "./images/cat.jpg";
+    pict.width=300;
+    frmRegister.btnRegister.value = "Register";
+    frmLogin.btnLogin.value = "Login";
+}
